5bf950effd982c2249dc6e31e1290523334cd1bd
Serzh C <chaikin.sergey@gmail.com>
2019-12-04 21:05:43 +0400

    Passing Props Up Search

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types';
 
 class Search extends Component {
     state = {
         text: ''
-    }
+    };
 
-    onChange = (e) => {
-        this.setState({ [e.target.name]: e.target.value})
-    }
+    static propTypes = {
+        searchUsers : PropTypes.func.isRequired,
+    };
 
     onSubmit = (e) => {
         e.preventDefault();
-        console.log(this.state.text);
-    }
+        this.props.searchUsers(this.state.text);
+        this.setState( {text: ''});
+    };
+
+    onChange = (e) => {
+        this.setState({ [e.target.name]: e.target.value})
+    };
 
     render() {
         return (
