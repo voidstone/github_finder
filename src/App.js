a10bcc5a64dc9cf9104de07822bce2991727e6c6
Serzh C <chaikin.sergey@gmail.com>
2019-12-08 23:23:06 +0400

    Single User Component Data

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Users from './components/users/Users';
 import axios from 'axios';
 import Alert from "./components/layout/Alert";
 import Search from './components/users/Search';
+import User from './components/users/User';
 import About from "./components/pages/About";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
@@ -13,6 +14,7 @@ class App extends Component  {
 
   state = {
     users : [],
+    user : {},
     loading: false,
     alert: null,
   };
@@ -24,10 +26,25 @@ class App extends Component  {
 
     const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=$
     {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
-    {process.env.REACT_APP_GITHUB_CLIENT_SECRET}}`);
+    {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
 
     this.setState( { users: res.data.items, loading :false })
   };
+
+  //Get a single Github user
+  getUser = async username => {
+    this.setState( { loading : true} );
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ user: res.data, loading: false });
+
+  };
+
 
   //Clear users from state
   clearUsers = () => {
@@ -44,7 +61,7 @@ class App extends Component  {
   
   render() {
 
-    const { users, loading } = this.state;
+    const { users, user, loading } = this.state;
     
     return (
       <Router>
@@ -66,7 +83,17 @@ class App extends Component  {
                   />
                 </Fragment>
               )}/>
-              <Route exact path='/about' component={About}/>
+              <Route exact path='/about' component={ About }/>
+              <Route
+                exact
+                path='/user/:login'
+                render={ props => (
+                  <User
+                        {...props }
+                        getUser={ this.getUser }
+                        user={ user }
+                        loading={ loading }/>
+                    ) } />
             </Switch>
 
             <Users loading={ loading } users={ users } />
